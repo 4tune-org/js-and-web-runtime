eab00f72def4f0438521d7a2bfa33b334d5442f2
eXory2024 <exory@anio.software>
2024-09-09 16:30:14 +0200

    Merge loadResource and loadStaticResource interfaces.

--- index.d.ts
+++ index.d.ts
@@ -28,30 +28,7 @@ interface LoadResourceFunction {
 }
 
 export const loadResource : LoadResourceFunction
-
-interface LoadStaticResourceFunction {
-	/**
-	 * @brief Synchronously load a resource
-	 * @description
-	 * Synchronously load the resource at `url`.
-	 * @param url URL of the resource.
-	 * @return
-	 * The resources content.
-	 */
-	(url : string) : ResourceReturnType
-
-	/**
-	 * @brief Synchronously load a resource
-	 * @description
-	 * Synchronously load the resource at `url`.
-	 * @param url URL of the resource.
-	 * @return
-	 * URL that points to the loaded resource.
-	 */
-	asURL(url : string) : string
-}
-
-export const loadStaticResource : LoadStaticResourceFunction
+export const loadStaticResource : LoadResourceFunction
 
 /**
  * @brief Dynamically load a resource
